d68aefe1c25f8f78468b2276039424e397027832
Edna <edulimaprog@gmail.com>
2019-08-23 17:00:01 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('adminlte::page')
+
+
+
+
+@section('content')
+
+
+		<div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p>Bounce Rate</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          
+		<div class="col-lg-3 col-6">
+			 <div class="small-box bg-success" style="width:80%; height:120px; background-color:#798a9c; margin: 0 auto">
+				<p style="color:white">dhfdiofhjuiosfiosfuoidusfoiufoidfs</p>
+			</div>
+		</div>
+
+
+		  
+		<div class="col-lg-3 col-6">
+			 <div class="small-box bg-success" style="width:80%; height:120px; background-color:#798a9c; margin: 0 auto">
+				<p style="color:white">dhfdiofhjuiosfiosfuoidusfoiufoidfs</p>
+			</div>
+		</div>
+    
+@stop
